826fe3f46c989002729e5f8e1e0c15328ef91cd3
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-07-09 14:51:37 +0200

    feat: add header with logo and title

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default{
+    name: "AppHeader",
+}
+
+</script>
+
+<template>
+    <div class="container">
+        <!-- logo -->
+        <div class="logo">
+            <img src="../assets/img/yugioh-logo-yu-gi-oh-11562857421zchdpnt0xu-122425415.png" alt="">
+        </div>
+        <!-- titolo -->
+         <h1>Yu-Gi-Oh Api</h1>
+    </div>
+    
+
+</template>
+
+
+<style lang="scss" scoped>
+
+.container{
+    padding: 20px;
+}
+
+.logo{
+    width: 100px;
+    padding-right: 20px;
+}
+
+</style>
